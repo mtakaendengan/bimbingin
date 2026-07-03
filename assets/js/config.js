bd160dccf9ef4926ea4d36ffe03e86ef8839bfb0
Mahardika Takaendengan <mahardika.takaendengan@gmail.com>
2026-07-03 16:29:50 +0800

    deleted

--- assets/js/config.js
+++
@@ -1,7 +0,0 @@
-window.BIMBINGIN_CONFIG = {
-  // Kosongkan dulu agar demo data tampil. Setelah Apps Script siap, isi dengan URL Web App /exec.
-  API_URL: "https://script.google.com/macros/s/AKfycbyl3oVXlBy1rgaqf_SGcXfZgJeYlfKzZtfQ-Ld8_8G8s386xS5HuFBf35o9NguC0ixs/exec",
-  APP_NAME: "BimbingIn",
-  DEMO_MODE_WHEN_API_EMPTY: true,
-  VERSION: "20260703-04"
-};
